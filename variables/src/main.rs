4fa4a653311c32cc950b56725859c4e942395910
yasuaki-344 <ikausay.443@gmail.com>
2021-10-04 21:20:29 +0900

    Add function

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    another_function();
+}
+
+fn another_function() {
+    println!("Another function.");  // 別の関数
 }
