1117a6569d0e3a4672228c2789691306096c9696
yasuaki-344 <ikausay.443@gmail.com>
2021-10-09 11:58:09 +0900

    Add TODO list to main.rs

--- band-calculation/src/main.rs
+++ band-calculation/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    // TODO: create tight-binding parameters
+    // TODO: create unit cell object
+    // TODO: create unit cell object
+    // TODO: generate output file
+    // TODO: wave number loop
+    // TODO: create Hamiltonian matrix
+    // TODO: solve eigen value problem of Hamiltonian matrix
+    // TODO: output eigen values to file
 }
