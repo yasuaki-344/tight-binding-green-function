511971c684dc671558a350c7438b5fa33c83fc9a
yasuaki-344 <ikausay.443@gmail.com>
2021-10-09 12:11:56 +0900

    Add wave number loop

--- band-calculation/src/main.rs
+++ band-calculation/src/main.rs
@@ -3,8 +3,11 @@ fn main() {
     // TODO: create unit cell object
     // TODO: create unit cell object
     // TODO: generate output file
-    // TODO: wave number loop
-    // TODO: create Hamiltonian matrix
-    // TODO: solve eigen value problem of Hamiltonian matrix
-    // TODO: output eigen values to file
+    let wave_number_point = 100;
+    for ik in 0..wave_number_point {
+        println!("wave number: {}", ik);
+        // TODO: create Hamiltonian matrix
+        // TODO: solve eigen value problem of Hamiltonian matrix
+        // TODO: output eigen values to file
+    }
 }
