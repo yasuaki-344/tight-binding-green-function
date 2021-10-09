53532b7b7c1e8e9dfc891606f30fc8307c3fefd0
yasuaki-344 <ikausay.443@gmail.com>
2021-10-09 15:59:40 +0900

    Update tight-binding parameter structure

--- band-calculation/src/main.rs
+++ band-calculation/src/main.rs
@@ -12,6 +12,65 @@ struct TightBindingOnsiteParameters {
     d: f64,
 }
 
+#[derive(Serialize, Deserialize)]
+struct TB_ScalingParameters {
+    /// hopping energy in no-strain.
+    h0: f64,
+    /// 1st scaling parameter.
+    n: f64,
+    /// 2nd scaling parameter.
+    nc: f64,
+    /// 3rd scaling parameter.
+    rc: f64,
+}
+
+/// Tight-Binding interaction parameters.
+#[derive(Serialize, Deserialize)]
+struct TB_InteractionParameters {
+    /// (ss sigma) element.
+    s_s_sigma: TB_ScalingParameters,
+    /// (ps sigma) element.
+    p_s_sigma: TB_ScalingParameters,
+    /// (pp sigma) element.
+    p_p_sigma: TB_ScalingParameters,
+    /// (pp pi) element.
+    p_p_pi: TB_ScalingParameters,
+    /// (s^*s sigma) element.
+    s_ast_s_sigma: TB_ScalingParameters,
+    /// (s^*p sigma) element.
+    s_ast_p_sigma: TB_ScalingParameters,
+    /// (s^*s^* sigma) element.
+    s_ast_s_ast_sigma: TB_ScalingParameters,
+    /// (ds sigma) element.
+    d_s_sigma: TB_ScalingParameters,
+    /// (ds^* sigma) element.
+    d_s_ast_sigma: TB_ScalingParameters,
+    /// (dp sigma) element.
+    d_p_sigma: TB_ScalingParameters,
+    /// (dp pi) element.
+    d_p_pi: TB_ScalingParameters,
+    /// (dd sigma) element.
+    d_d_sigma: TB_ScalingParameters,
+    /// (dd pi) element.
+    d_d_pi: TB_ScalingParameters,
+    /// (dd delta) element.
+    d_d_delta: TB_ScalingParameters,
+}
+
+#[derive(Serialize, Deserialize)]
+struct TightBindingParameter {
+    /// distance between nearest neighboring atoms in bulk crystal [Ang].
+    atomic_distance: f64,
+    /// Tight-Binding Onsite Parameter of anion.
+    onsite_anion: TightBindingOnsiteParameters,
+    /// Tight-Binding Onsite Parameter of cation.
+    onsite_cation: TightBindingOnsiteParameters,
+    /// Tight-Binding Overlap Parameter of the transition from anion.
+    overlap_from_anion: TB_InteractionParameters,
+    /// Tight-Binding Overlap Parameter of the transition from cation.
+    overlap_from_cation: TB_InteractionParameters,
+}
+
 fn main() {
     // TODO: create tight-binding parameters
     let tb_parameter = TightBindingOnsiteParameters {
