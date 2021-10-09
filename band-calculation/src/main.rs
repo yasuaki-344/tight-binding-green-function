0f0cb05cda6d6fbb036ae8f4af160f22f7157d93
yasuaki-344 <ikausay.443@gmail.com>
2021-10-09 12:34:51 +0900

    Creaate TightBindingOnsiteParameters

--- band-calculation/src/main.rs
+++ band-calculation/src/main.rs
@@ -1,3 +1,14 @@
+struct TightBindingOnsiteParameters {
+    /// s-orbit onsite energy parameter.
+    s: f64,
+    /// p-orbit onsite energy parameter.
+    p: f64,
+    /// s*-orbit onsite energy parameter.
+    s_ast: f64,
+    /// d-orbit onsite energy parameter.
+    d: f64,
+}
+
 fn main() {
     // TODO: create tight-binding parameters
     // TODO: create unit cell object
