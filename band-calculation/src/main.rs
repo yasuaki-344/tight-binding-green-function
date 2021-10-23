44b281c77abf3beab0bfa894f076be2d2d8895c3
yasuaki-344 <ikausay.443@gmail.com>
2021-10-23 16:17:56 +0900

    Implement band calculation process

--- band-calculation/src/main.rs
+++ band-calculation/src/main.rs
@@ -8,7 +8,7 @@ use plotters::prelude::*;
 
 const OUT_FILE_NAME: &'static str = "band-structure.png";
 
-fn plot_data() -> Result<(), Box<dyn std::error::Error>> {
+fn plot_data(x: Vec<f32>, y1: Vec<f32>, y2: Vec<f32>) -> Result<(), Box<dyn std::error::Error>> {
     let image_width = 1080;
     let image_height = 720;
 
@@ -16,8 +16,6 @@ fn plot_data() -> Result<(), Box<dyn std::error::Error>> {
         BitMapBackend::new(OUT_FILE_NAME, (image_width, image_height)).into_drawing_area();
 
     root_area.fill(&WHITE)?;
-
-    let x_axis = (0f32..std::f32::consts::PI).step(0.01 * std::f32::consts::PI);
 
     let mut cc = ChartBuilder::on(&root_area)
         .margin(5)
@@ -33,18 +31,14 @@ fn plot_data() -> Result<(), Box<dyn std::error::Error>> {
         .draw()?;
 
     // plot data
-    let _ = cc.draw_series(LineSeries::new(
-        x_axis
-            .values()
-            .map(|x| (x, -2.0 + (2.0 + 2.0 * x.cos()).sqrt())),
+    cc.draw_series(LineSeries::new(
+        x.iter().zip(y1.iter()).map(|(x, y)| (*x, *y)),
         &RED,
-    ));
-    let _ = cc.draw_series(LineSeries::new(
-        x_axis
-            .values()
-            .map(|x| (x, -2.0 - (2.0 + 2.0 * x.cos()).sqrt())),
+    ))?;
+    cc.draw_series(LineSeries::new(
+        x.iter().zip(y2.iter()).map(|(x, y)| (*x, *y)),
         &RED,
-    ));
+    ))?;
 
     // save plot data as image file
     root_area.present().expect("Unable to write result to file");
@@ -53,17 +47,22 @@ fn plot_data() -> Result<(), Box<dyn std::error::Error>> {
 
 fn main() {
     let wave_number_point = 100;
+    let mut x = Vec::new();
+    let mut y1 = Vec::new();
+    let mut y2 = Vec::new();
     for ik in 0..wave_number_point {
-        let wave_number = (ik as f64) * (1.0 / std::f64::consts::PI);
+        let wave_number = (ik as f32) * std::f32::consts::PI / (wave_number_point as f32 - 1.0);
         println!("wave number: {}", wave_number);
+        x.push(wave_number);
         // TODO: create Hamiltonian matrix
         let hamiltonian = arr2(&[
-            [c64::new(-2.0, 0.0), 1.0 + c64::new(0., wave_number).exp()],
-            [1.0 + c64::new(0., -wave_number).exp(), c64::new(-2.0, 0.0)],
+            [c32::new(-2.0, 0.0), 1.0 + c32::new(0., wave_number).exp()],
+            [1.0 + c32::new(0., -wave_number).exp(), c32::new(-2.0, 0.0)],
         ]);
         let (eigen, _) = hamiltonian.clone().eigh(UPLO::Upper).unwrap();
-        println!("eigenvalues = \n{:?}", eigen);
+        y1.push(eigen[0]);
+        y2.push(eigen[1]);
     }
     // TODO: output eigen values to file
-    let _ = plot_data();
+    let _ = plot_data(x, y1, y2);
 }
